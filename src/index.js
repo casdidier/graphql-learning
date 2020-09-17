aab64a03b1b55797149ce6c966435969f015c7e9
casdidier <cheung.didier@gmail.com>
2020-09-17 12:12:09 +0200

    added login and userInfo

--- src/index.js
+++ src/index.js
@@ -1,51 +1,70 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, gql } = require("apollo-server");
+
+// type checking
+// query vs. mutation
+// objects
+// arrays
+// arguments
+
+// crud
 
 const typeDefs = gql`
-type Query {
-  hello: String!
-}
-
-type User {
-  id: ID!
-  username: String!
-}
-
-type Error {
-  field: String!
-  message: String!
-}
-
-type RegisterResponse {
-  errors: [Error]
-  user: User!
-
-}
-
-type Mutation {
- register: RegisterResponse!
-}
+  type Query {
+    hello: String
+    user: User
+  }
+  type User {
+    id: ID!
+    username: String!
+  }
+  type Error {
+    field: String!
+    message: String!
+  }
+  type RegisterResponse {
+    errors: [Error!]!
+    user: User
+  }
+  input UserInfo {
+    username: String!
+    password: String!
+    age: Int
+  }
+  type Mutation {
+    register(userInfo: UserInfo!): RegisterResponse!
+    login(userInfo: UserInfo!): Boolean!
+  }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'hello world'
+    hello: () => null,
+    user: () => ({
+      id: 1,
+      username: "bob"
+    })
   },
-
-  // type CRUD
   Mutation: {
+    login: () => true,
     register: () => ({
-      errors: [{
-        field: 'username',
-        message: 'bad'
-      }],
+      errors: [
+        {
+          field: "username",
+          message: "bad"
+        },
+        {
+          field: "username2",
+          message: "bad2"
+        }
+      ],
       user: {
         id: 1,
         username: "bob"
       }
     })
   }
-}
+};
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => console.log(`server started at  ${url}`));
+server.listen().then(({ url }) => console.log(`server started at ${url}`));
